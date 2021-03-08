a9310c9d9d4e9e1db7f0fdb0ba16ad9b7688123f
zulfarizqi <bayufirmansyah2800>
2021-03-08 22:19:41 +0700

    seeting routes di server js

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const app = express();
 // melakukan parse application/json
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// panggil routes
+let routes = require('./routes');
+routes(app);
+
+
 app.listen(3000, () => {
     console.log(`Server started on port`);
 });
